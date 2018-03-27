0180467bdc77b39727dc4da4b02d1fc52de8ef02
Henry Xu <henry.xu@ringcentral.com>
2018-03-27 14:08:08 +0800

    handling error properly

--- device_lab/handlers/base.py
+++ device_lab/handlers/base.py
@@ -1,5 +1,17 @@
+import json
+import traceback
+
 import tornado.web
 
 
 class BaseHandler(tornado.web.RequestHandler):
-    pass
+    def write_error(self, status_code, **kwargs):
+        self.set_header('Content-Type', 'application/json')
+        err_res = {
+            'code': status_code,
+            'message': self._reason,
+        }
+        if self.settings.get("serve_traceback") and "exc_info" in kwargs:
+            # in debug mode, try to send a traceback
+            err_res['traceback'] = traceback.format_exception(*kwargs["exc_info"])
+        self.finish(json.dumps(err_res))
--- device_lab/handlers/selenium_grid.py
+++ device_lab/handlers/selenium_grid.py
@@ -9,5 +9,12 @@ from services.selenium_grid import selenium_grid_service
 class SeleniumGridListHandler(BaseHandler):
     @tornado.gen.coroutine
     def get(self):
-        devices = selenium_grid_service.get_devices()
+        devices = selenium_grid_service.get_devices(
+            platform_name=self.get_argument('platform_name'),
+            platform_version=self.get_argument('platform_version', None),
+            platform_version_gt=self.get_argument('platform_version_gt', None),
+            platform_version_gte=self.get_argument('platform_version_gte', None),
+            platform_version_lt=self.get_argument('platform_version_lt', None),
+            platform_version_lte=self.get_argument('platform_version_lte', None),
+        )
         self.write(json.dumps(devices))
