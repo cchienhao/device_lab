f73dced5860ff9e7086334e5a6a9370f497101a6
Henry Xu <henry.xu@ringcentral.com>
2018-03-28 15:39:26 +0800

    handle CORS properly

--- device_lab/handlers/base.py
+++ device_lab/handlers/base.py
@@ -5,6 +5,15 @@ import tornado.web
 
 
 class BaseHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
     def get_error_message(self, **kwargs):
         if 'exc_info' not in kwargs:
             return self._reason
