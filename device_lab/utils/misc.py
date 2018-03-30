568ebfcccfc2b215008e9775be05ba8abed74da1
Henry Xu <henry.xu@ringcentral.com>
2018-03-30 19:38:53 +0800

    decorator to handle exception

--- device_lab/utils/misc.py
+++ device_lab/utils/misc.py
@@ -1,8 +1,23 @@
 import string
 import secrets
+from functools import wraps
 
 default_alphabet = string.ascii_letters + string.digits
 
 
 def new_random_string(size, alphabet=default_alphabet):
-    return ''.join(secrets.choice(alphabet) for _ in range(20))
+    return ''.join(secrets.choice(alphabet) for _ in range(size))
+
+
+def on_exception_return(value_or_callable):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except Exception as e:
+                if callable(value_or_callable):
+                    return value_or_callable(e)
+                return value_or_callable
+        return wrapper
+    return decorator
