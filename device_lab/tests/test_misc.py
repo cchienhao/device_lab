1960b98dbb5d99d5a2b6ce3325f5925a127719cf
Henry Xu <henry.xu@ringcentral.com>
2018-03-30 19:40:53 +0800

    some test

---
+++ device_lab/tests/test_misc.py
@@ -0,0 +1,18 @@
+import unittest
+
+from utils.misc import on_exception_return
+
+
+class TestOnExceptionReturn(unittest.TestCase):
+    def test_on_error_exception_return_value(self):
+        @on_exception_return(None)
+        def raise_exception(msg):
+            raise Exception(msg)
+        self.assertIsNone(raise_exception('bad things happen'))
+
+    def test_on_error_exception_return_callable(self):
+        @on_exception_return(lambda e: str(e))
+        def raise_exception(msg):
+            raise Exception(msg)
+        msg = 'bad things happen'
+        self.assertEqual(msg, raise_exception(msg))
